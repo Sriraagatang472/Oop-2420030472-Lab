038a16d6426e610e76329e45a3e4840cb481f39b
Sriraagatang472 <2420030472@klh.edu.in>
2025-08-04 09:18:18 +0530

    Add files via upload

---
+++ gcd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class gcd {
+public static void main(String args[]){
+	Scanner scanner= new Scanner(System.in);
+	System.out.print("Enter first number:");
+	int a=scanner.nextInt();
+	System.out.print("Enter second number:");
+	int b=scanner.nextInt();
+	 while (b != 0) {
+         int temp = b;
+         b = a % b;
+         a = temp;
+     }
+	System.out.println(a);
+}
+}
---
+++ sumofdigits.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class sumofdigits {
+public static void main(String args[]) {
+	Scanner scanner=new Scanner(System.in);
+	System.out.print("Enter number:");
+	int number=scanner.nextInt();
+		
+	int sum=0;
+	int temp=number;
+	
+	while(temp!=0) {
+		int digit=temp%10;
+		sum+=digit;
+		temp/=10;
+		
+	}
+	System.out.print(sum);
+}
+}
